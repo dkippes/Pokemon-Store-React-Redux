90398aa6b5eae0198b4d10c773d9d01ee721b0dd
dkippes <kippes.diego@gmail.com>
2021-03-24 00:58:40 -0300

    react-redux node-redux practice

---
+++ Pokemon-Store(React-Redux)/src/App.js
@@ -0,0 +1,48 @@
+import img from './img/pokeimg.jpg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import CantidadPokemonHook from './components/CantidadPokemon.hook';
+import CompraPokemonHook from './components/CompraPokemon.hook';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import BuscadorPokemon from './components/buscador/BuscadorPokemon';
+import ResultadoPokemon from './components/buscador/ResultadoPokemon';
+
+function App() {
+
+  return (
+	  <Provider store={store}>
+		<div className="App container">
+			<div className="row">
+				<div className="col-12">
+
+					<div className="card mt-5" style={{maxWidth: '370px'}}>
+						<div className="col-4">
+							<img src={img} alt="pokemon" className="card-img" />
+						</div>
+						<div className="col-8">
+							<div className="card-body">
+								<div className="card-title h3 text-center">
+									<CantidadPokemonHook />
+								</div>
+								<CompraPokemonHook />
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-12 mt-4 border-top pt-3">
+					<BuscadorPokemon />
+				</div>
+				<div className="col-12">
+					<ResultadoPokemon />
+				</div>
+			</div>
+		</div>
+	</Provider>
+  );
+}
+
+export default App;
---
+++ Pokemon-Store(React-Redux)/src/components/CantidadPokemon.hook.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect, useSelector } from 'react-redux';
+
+const CantidadPokemonHook = () => {
+
+	const game_shop = useSelector(state => state.game_shop)
+
+	return (
+		<React.Fragment>
+			Unidades: {game_shop.pokemon}
+		</React.Fragment>
+	)
+}
+
+
+
+export default CantidadPokemonHook;
+
